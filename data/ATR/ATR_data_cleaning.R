eaa4536d6b279ab060d67a7dbc218872e7bc4594
Luis Gentner <luis.gentner@outlook.com>
2023-05-18 11:39:58 +0200

    English sector names

--- data/ATR/ATR_data_cleaning.R
+++ data/ATR/ATR_data_cleaning.R
@@ -6,20 +6,30 @@ library(ESdata)
 
 # Import data
 atr <- read_excel("data/ATR/ATR_I_2009-2021_clean.xlsx",
-           sheet = "ATR-I.1.3",
-           range = "A7:O102")
+                  sheet = "ATR-I.1.3",
+                  range = "A7:O102")
 
 # Autonomous communities according to ESdata::ccaa_iso
-renamed_ccaa = c("España" = "total",
-                 "Principado de Asturias" = "Asturias (Principado de)",
-                 "Illes Balears" = "Balears (Illes)",
-                 "Castilla - La Mancha" = "Castilla-La Mancha",
-                 "Catalunya" = "Cataluña",
-                 "Comunidad Valenciana" = "Comunitat Valenciana",
-                 "Comunidad de Madrid" = "Madrid (Comunidad de)",
-                 "Región de Murcia" = "Murcia (Región de)",
-                 "Comunidad Foral de Navarra" = "Navarra (Comunidad Foral de)",
-                 "La Rioja" = "Rioja (La)")
+renamed_ccaa = c(
+  "España" = "total",
+  "Principado de Asturias" = "Asturias (Principado de)",
+  "Illes Balears" = "Balears (Illes)",
+  "Castilla - La Mancha" = "Castilla-La Mancha",
+  "Catalunya" = "Cataluña",
+  "Comunidad Valenciana" = "Comunitat Valenciana",
+  "Comunidad de Madrid" = "Madrid (Comunidad de)",
+  "Región de Murcia" = "Murcia (Región de)",
+  "Comunidad Foral de Navarra" = "Navarra (Comunidad Foral de)",
+  "La Rioja" = "Rioja (La)"
+)
+# Sector in English
+renamed_sector = c(
+  "Total" = "total",
+  "Agriculture" = "agrario",
+  "Industry" = "industria",
+  "Construction" = "construccion",
+  "Services" = "servicios"
+)
 
 # Name and ISO label of autonomous communities
 ccaa_iso <- ccaa_iso |>
@@ -30,8 +40,10 @@ atr <- atr |>
   mutate(across(c(sector, region), as_factor)) |>
   # Drop autonomous cities
   filter(region != "Ceuta y Melilla") |>
-  # Rename regions according to ccaa_iso and join with ISO names
-  mutate(region = fct_recode(region, !!!renamed_ccaa)) |>
+  # Rename regions and sectors
+  mutate(region = fct_recode(region,!!!renamed_ccaa),
+         sector = fct_recode(sector, !!!renamed_sector)) |>
+  # Join with ISO names
   left_join(ccaa_iso,
             by = join_by(region == nombres)) |>
   # Rename region columns
@@ -55,4 +67,3 @@ atr <- atr |>
 save(atr, file = "data/ATR/ATR-I.1.3.RData")
 
 atr
-
